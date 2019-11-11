509dc7fb6fab0b9a64dd6d00c4ee727ddb70c4c9
Gurupratap <gurupratap.matharu@gmail.com>
2019-11-11 14:00:48 -0300

    separate widget to create a task list

--- lib/widgets/tasks_list.dart
+++ lib/widgets/tasks_list.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:todoey_flutter/models/task.dart';
 import 'task_tile.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
+  final List<Task> tasks;
+
+  TasksList({this.tasks});
+
+  @override
+  _TasksListState createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: <Widget>[
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-        TaskTile(),
-      ],
+    return ListView.builder(
+      itemCount: widget.tasks.length,
+      itemBuilder: (context, index) {
+        return TaskTile(
+          taskTitle: widget.tasks[index].title,
+          isChecked: widget.tasks[index].isDone,
+          toggleTask: (checkBoxState) {
+            setState(() {
+              widget.tasks[index].toggleTask();
+            });
+          },
+        );
+      },
     );
   }
 }
