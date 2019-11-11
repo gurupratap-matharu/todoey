859c81ea16bda9a29f36ef7c95f57f6b011ee9f2
Gurupratap <gurupratap.matharu@gmail.com>
2019-11-11 14:00:30 -0300

    modular widget to create each task as a tile widget

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,33 +1,25 @@
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
+  TaskTile({this.taskTitle, this.isChecked, this.toggleTask});
+
+  final bool isChecked;
+  final String taskTitle;
+  final Function toggleTask;
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Mow the lawn'),
-      trailing: TaskCheckbox(),
-    );
-  }
-}
-
-class TaskCheckbox extends StatefulWidget {
-  @override
-  _TaskCheckboxState createState() => _TaskCheckboxState();
-}
-
-class _TaskCheckboxState extends State<TaskCheckbox> {
-  bool isChecked = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-      activeColor: Colors.lightBlueAccent,
-      value: isChecked,
-      onChanged: (newValue) {
-        setState(() {
-          isChecked = newValue;
-        });
-      },
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: toggleTask,
+      ),
     );
   }
 }
